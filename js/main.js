7e8c44c4941aee7db37ce5d332783ba00d8bb15c
Your Name <you@example.com>
2016-05-02 18:51:14 -0700

    2nd draft changes

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// When user hovers over anything with the class .pics, add a hover state 
+  // add a css class to all the <a> tags
+
+// When user clicks on main slider banner with the class .fancybox clearfix, scroll images in a carousel
+  // ?? 
+  // Need to figure out how to do this - unrelated, this isn't pseudo code
+
+  
